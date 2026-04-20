02750064f2bd8866f4cff492018f69432151bb31
Tanel Põld <tanel@brightside.ee>
2026-04-20 11:20:19 +0300

    Update Automation_Custom_Script.sh

--- dietpi_helpers/Automation_Custom_Script.sh
+++ dietpi_helpers/Automation_Custom_Script.sh
@@ -117,7 +117,7 @@ apt-get install -y \
     git build-essential autoconf automake libtool pkg-config \
     libasound2-dev libbluetooth-dev libglib2.0-dev libsbc-dev \
     libfdk-aac-dev libfreeaptx-dev libldacbt-enc-dev libldacbt-abr-dev \
-    libdbus-1-dev libsystemd-dev
+    libdbus-1-dev libsystemd-dev smbclient
 
 # 4. Compile BlueALSA
 echo "=> Building Custom BlueALSA with AAC..."
