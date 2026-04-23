d859361522f2008ca667d7ae90d91ddbfb3cc2fe
Tanel Põld <tanel@brightside.ee>
2026-04-23 10:10:53 +0300

    Install rclone and fuse3 for cloud storage
    Added installation of rclone and fuse3 packages for cloud storage support.

--- dietpi_helpers/Automation_Custom_Script.sh
+++ dietpi_helpers/Automation_Custom_Script.sh
@@ -107,6 +107,7 @@ wait_for_apt
 apt-get update
 wait_for_apt
 apt-get install -y \
+    rclone fuse3 \
     libbluetooth3 libsbc1 libfreeaptx0 libldacbt-enc2 libldacbt-abr2 \
     libgirepository-2.0-0 gir1.2-glib-2.0 python3-gi \
     avahi-daemon alsa-utils bluez bluez-tools rfkill dbus \
@@ -118,6 +119,9 @@ apt-get install -y \
     libasound2-dev libbluetooth-dev libglib2.0-dev libsbc-dev \
     libfdk-aac-dev libfreeaptx-dev libldacbt-enc-dev libldacbt-abr-dev \
     libdbus-1-dev libsystemd-dev smbclient cifs-utils udisks2 id3v2
+
+echo "=> Configuring FUSE permissions for Cloud Storage..."
+sed -i 's/#user_allow_other/user_allow_other/g' /etc/fuse.conf
 
 # 4. Compile BlueALSA
 echo "=> Building Custom BlueALSA with AAC..."
